340b0a05f75a2b48b811f12c21209744e0e2bc5c
Oktikk <65051521+Oktikk@users.noreply.github.com>
2023-04-29 16:03:59 +0200

    distance display

--- ProjectGK/Assets/_Scripts/Monobehaviours/PlayerController.cs
+++ ProjectGK/Assets/_Scripts/Monobehaviours/PlayerController.cs
@@ -7,50 +7,54 @@ public class PlayerController : MonoBehaviour
 {
     private Rigidbody _rb;
     private float _moveHorizontal;
-    private float _moveVertical;
 
     private float _movementSpeed;
 
-    Vector3 forward;
     Vector3 right;
-    Vector3 forwardRelativeVertical;
     Vector3 rightRelativeHorizontal;
+
+    private Vector3 _currentVelocity;
 
 
     private void Start()
     {
         _rb = GetComponent<Rigidbody>();
-        _movementSpeed = 2f;
+        _movementSpeed = 1f;
     }
 
 
     private void FixedUpdate()
     {
-        //MoveRelativeToCamera();
+        MoveRelativeToCamera();
     }
 
     private void MoveRelativeToCamera()
     {
         _moveHorizontal = Input.GetAxisRaw("Horizontal");
-        _moveVertical = Input.GetAxisRaw("Vertical");
 
-        forward = Camera.main.transform.forward;
         right = Camera.main.transform.right;
 
-        forward.y = 0;
         right.y = 0;
 
-        forward = forward.normalized;
         right = right.normalized;
 
-        forwardRelativeVertical = _moveVertical * forward;
         rightRelativeHorizontal = _moveHorizontal * right;
 
-        Vector3 cameraRelativeMovement = forwardRelativeVertical + rightRelativeHorizontal;
+        Vector3 cameraRelativeMovement = rightRelativeHorizontal * _movementSpeed;
 
-        if (_moveHorizontal != 0f || _moveVertical != 0f)
+        if (_moveHorizontal != 0f)
         {
-            _rb.AddForce(cameraRelativeMovement * _movementSpeed);
+            _currentVelocity = _rb.velocity;
+            _currentVelocity.x = 0;
+            _currentVelocity += cameraRelativeMovement;
+            _rb.velocity = _currentVelocity;
         }
+        else
+        {
+            _currentVelocity = _rb.velocity;
+            _currentVelocity.x = 0;
+            _rb.velocity = _currentVelocity;
+        }
+        Debug.Log(_rb.velocity);
     }
 }
